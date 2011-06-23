75e812c561df8bb793f9c9a94442be4eb96ee164
Michael Nelson <dekarrin@mail.com>
2011-06-23 15:42:32 -0500

    Added a few more chunks

--- src/com/dekarrin/file/png/InternationalTextDataChunk.java
+++ src/com/dekarrin/file/png/InternationalTextDataChunk.java
@@ -8,12 +8,97 @@ public class InternationalTextDataChunk extends TextChunk implements AncillaryCh
 	/**
 	 * Whether the text data contained is compressed.
 	 */
-	private boolean isCompressed;
+	private boolean compressed;
 	
 	/**
 	 * Which compression method is used.
 	 */
 	private byte compressionMethod;
 	
+	/**
+	 * The language of this string.
+	 */
+	private String languageTag;
 	
+	/**
+	 * The keyword translated.
+	 */
+	private String translatedKeyword;
+	
+	/**
+	 * Creates a new InternationalTextDataChunk.
+	 *
+	 * @param data
+	 * The chunk data.
+	 *
+	 * @param crc
+	 * The chunk CRC.
+	 */
+	public InternationalTextDataChunk(byte[] data, int crc) {
+		super(new byte[]{105, 84, 88, 116}, data, crc);
+		parseData();
+	}
+	
+	/**
+	 * Checks whether this chunk is compressed.
+	 *
+	 * @returns
+	 * Whether it is compressed.
+	 */
+	public boolean isCompressed() {
+		return compressed;
+	}
+	
+	/**
+	 * Gets the compression method.
+	 *
+	 * @returns
+	 * The compression method.
+	 */
+	public byte getCompressionMethod() {
+		return compressionMethod;
+	}
+	
+	/**
+	 * Gets the text of this chunk.
+	 */
+	public String getText() {
+		return text;
+	}
+	
+	/**
+	 * Gets the language tag.
+	 *
+	 * @returns
+	 * The language tag.
+	 */
+	public String getLanguageTag() {
+		return languageTag;
+	}
+	
+	/**
+	 * Gets the translated keyword.
+	 *
+	 * @returns
+	 * The keyword.
+	 */
+	public String getTranslatedKeyword() {
+		return translatedKeyword;
+	}
+	
+	/**
+	 * Parses chunk data into usable data.
+	 */
+	private void parseData() {
+		keyword = parseString();
+		compressed = (parseByte() == 1);
+		compressionMethod = parseByte();
+		languageTag = parseString();
+		translatedKeyword = parseString();
+		text = parseFinalString();
+		if(compressed) {
+			ZlibDecompresser zd = new ZlibDecompresser(text);
+			text = zd.decompressString("UTF-8");
+		}
+	}
 }
